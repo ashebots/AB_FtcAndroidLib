6ee42a08a2a7510541b1f84be61d250abafc0170
Nolan <nolan.scobie@gmail.com>
2015-08-13 21:11:41 -0400

    Basic driving class. Needs work

---
+++ FtcAndroidLib/src/main/java/org/ashebots/ftcandroidlib/drive/ChassisArcade.java
@@ -0,0 +1,27 @@
+package org.ashebots.ftcandroidlib.drive;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+public class ChassisArcade extends BaseChassis {
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+
+    public ChassisArcade(DcMotor leftMotor, DcMotor rightMotor)
+    {
+        this.leftMotor = leftMotor;
+        this.rightMotor = rightMotor;
+    }
+
+    public void Drive(float axisX, float axisY)
+    {
+        axisX = Range.clip(axisX, -1.0f, 1.0f);
+        axisY = Range.clip(axisY, -1.0f, 1.0f);
+
+        double leftPower = Range.clip(axisY + axisX, -1.0f, 1.0f);
+        double rightPower = Range.clip(axisY - axisX, -1.0f, 1.0f);
+
+        this.leftMotor.setPower(leftPower);
+        this.rightMotor.setPower(rightPower);
+    }
+}
