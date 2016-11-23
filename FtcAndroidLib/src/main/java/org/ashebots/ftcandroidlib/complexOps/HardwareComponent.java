ff82f00e193b1c00ceaf10fe0c8473fb21765a20
Nolan <nolan.scobie@gmail.com>
2016-11-22 21:01:36 -0500

    Add telemetry to HadwareComponent

--- FtcAndroidLib/src/main/java/org/ashebots/ftcandroidlib/complexOps/HardwareComponent.java
+++ FtcAndroidLib/src/main/java/org/ashebots/ftcandroidlib/complexOps/HardwareComponent.java
@@ -1,7 +1,23 @@
 package org.ashebots.ftcandroidlib.complexOps;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 //This class is used to provide a base for all hardware, sensor, and computer functions - for example motors or timers.
 public abstract class HardwareComponent {
+
+    private Telemetry telemetry = null;
+    //Must give telemetry object before it can be used
+    public void SetTelemetry(Telemetry t){
+        telemetry = t;
+    }
+    //Add data to return with telemetry
+    // if there is no telemetry object, do nothing and return null
+    protected Telemetry.Item AddTelemertyData (String caption, Object value){
+        if(telemetry != null){
+            return(telemetry.addData(caption, value));
+        }
+        return (null);
+    }
     //Calibrate stores the current encoder position to use in relative encoder tracking.
     public void calibrate() {
     }
